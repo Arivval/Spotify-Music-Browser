af3b42637b83fd774e68a2fb699ad9e75e8afd8f
Yilin Ma <yilinma3@illinois.edu>
2019-09-30 21:20:42 -0500

    Updates

---
+++ precache-manifest.578f3fa551d6e7c37b9464ca5dee96d3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Spotify-Music-Browser/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0e1aed9d813ee1c81d52",
+    "url": "/Spotify-Music-Browser/static/css/main.3cc533ae.chunk.css"
+  },
+  {
+    "revision": "b2b9ba2ea373e1c28644",
+    "url": "/Spotify-Music-Browser/static/js/runtime~main.b2b9ba2e.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Spotify-Music-Browser/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ec6566a7e641126def7a",
+    "url": "/Spotify-Music-Browser/static/js/2.ec6566a7.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Spotify-Music-Browser/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Spotify-Music-Browser/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Spotify-Music-Browser/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Spotify-Music-Browser/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Spotify-Music-Browser/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Spotify-Music-Browser/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "0e1aed9d813ee1c81d52",
+    "url": "/Spotify-Music-Browser/static/js/main.0e1aed9d.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Spotify-Music-Browser/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Spotify-Music-Browser/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Spotify-Music-Browser/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Spotify-Music-Browser/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Spotify-Music-Browser/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Spotify-Music-Browser/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Spotify-Music-Browser/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Spotify-Music-Browser/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ec6566a7e641126def7a",
+    "url": "/Spotify-Music-Browser/static/css/2.79e5ea48.chunk.css"
+  },
+  {
+    "revision": "db62395bce50edfab6b15d72598b86bf",
+    "url": "/Spotify-Music-Browser/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Spotfiy-Music-Browser/precache-manifest.81d620941c516682f8af83a9463b39fa.js"
+  "/Spotify-Music-Browser/precache-manifest.578f3fa551d6e7c37b9464ca5dee96d3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Spotfiy-Music-Browser/index.html", {
+workbox.routing.registerNavigationRoute("/Spotify-Music-Browser/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
